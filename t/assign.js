8e011f0ecabef3b998a9c61dcebe983d3762dede
Bas Huis <bas080@hotmail.com>
2022-03-14 11:09:22 +0200

    Add tests

---
+++ t/assign.js
@@ -0,0 +1,3 @@
+john = registerPerson("John", "Jiggles");
+jesse = registerPerson("Jesse", "James");
+jane = registerPerson("Jane", "Joker");
---
+++ t/tests.t
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use Test::More tests => 1;
+
+begin;
+
+is(1 + 1,  2, "Basic addition is working");
+
